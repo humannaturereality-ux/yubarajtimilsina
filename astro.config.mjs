bb63cdc3a58d436f26587e2424a924d9422aa47e
Yubaraj Timilsina <human.nature.reality@gmail.com>
2026-02-04 11:22:03 +0545

    Updated the astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  output: "static",   // <-- add this line
   vite: {
     plugins: [tailwindcss()]
   }
